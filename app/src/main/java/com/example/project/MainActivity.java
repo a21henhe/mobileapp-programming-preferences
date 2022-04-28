365f474d2487142f7e60dc64c5100deeab93fc02
a21henhe <a21henhe@tudent.his.se>
2022-04-28 16:12:24 +0200

    read shared preferences and printed to textview

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private SharedPreferences prefs;
+    private TextView prefText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        prefs = getPreferences(MODE_PRIVATE);
+
+        prefText = findViewById(R.id.prefText);
+        prefText.setText(prefs.getString("MyAppPreferenceString", "No pref found"));
+
     }
 
 }
