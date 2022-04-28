4f3844a4aff11e3fb0318fa553f9cfeca5c51df7
a21henhe <a21henhe@tudent.his.se>
2022-04-28 16:23:11 +0200

    added second activity and intent to navigate to it

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences prefs;
     private TextView prefText;
+    private Button navigateToSecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
         prefText = findViewById(R.id.prefText);
         prefText.setText(prefs.getString("MyAppPreferenceString", "No pref found"));
 
+        navigateToSecondActivity = findViewById(R.id.navigateToSecondActivity);
+        navigateToSecondActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                onButtonClick();
+            }
+        });
+
+
+    }
+
+    private void onButtonClick() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 
 }
